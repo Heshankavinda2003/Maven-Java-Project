b2c0f97a4d75ba898b3c169ab6487b16e4d8f26a
heshankav <heshankavinda2023@gmail.com>
2026-02-10 00:38:04 +0530

    Second commit

--- src/main/java/com/student/db/DBConnector.java
+++ src/main/java/com/student/db/DBConnector.java
@@ -1,31 +1,24 @@
 package com.student.db;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnector {
 
     private static Connection connection = null;
-
     public static Connection getConnection() {
-
         try {
             if (connection == null) {
-
                 Class.forName("com.mysql.cj.jdbc.Driver");
-
                 connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/student_management_system",
                         "root",
                         "r00t"
                 );
-
-                System.out.println("✅ Database Connected");
+                System.out.println("..........Database Connected..........");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return connection;
     }
 }
